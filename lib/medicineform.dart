12b3d3dcf6ff1a8464578e57c8488545b99f53a1
ashutosh9119 <asmednipur@gmail.com>
2021-11-13 12:15:12 +0530

    medicine form

---
+++ lib/medicineform.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+class MedicineForm extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text("Dawa Lo"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextField(
+              decoration: const InputDecoration(
+                  border: OutlineInputBorder(), hintText: 'Disease Name'),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Add Medicine",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            TextField(
+              decoration: const InputDecoration(
+                  border: OutlineInputBorder(), hintText: 'Medicine'),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextField(
+              decoration: const InputDecoration(
+                  border: OutlineInputBorder(), hintText: "Medicine"),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextField(
+              decoration: const InputDecoration(
+                  border: OutlineInputBorder(), hintText: 'Medicine'),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.green,
+                  boxShadow: [
+                    BoxShadow(
+                        color: (Colors.green[700])!,
+                        blurRadius: 50,
+                        offset: Offset(0, 0),
+                        spreadRadius: 5)
+                  ]),
+              width: double.infinity,
+              child: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.add,
+                  size: 60,
+                  color: Colors.white,
+                ),
+              ),
+              height: 80,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
